87d2a73faca34ac53d9da91afd2a34e4d4610ff9
rocapa18 <roberto.pavano@gmail.com>
2019-11-16 17:35:04 -0300

    Primer Copia

---
+++ routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/productos', function () {
+    return view('productos');
+});
+
+Route::get('/detalle', function () {
+    return view('detalle');
+});
+
+Route::get('/registro', function () {
+    return view('registro');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/contacto', function () {
+    return view('contacto');
+});
+
+Route::get('/faq', 'faqController@listado');
+
+Route::get('/carrito', function () {
+    return view('carrito');
+});
+
+Route::get('/recuperar', function () {
+    return view('recuperar');
+});
